9691a8cdefb415459c75aa7417a46c2681a99adf
Ketan-Goswami <ketangoswami2003@gmail.com>
2025-02-26 17:25:26 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+    const Voting = await hre.ethers.getContractFactory("Voting");
+    const Voting_ = await Voting.deploy(["Mark", "Mike", "George"], 30);
+
+    await Voting_.deployed();
+
+    console.log(
+        `Contract Address : ${Voting_.address}`
+    );
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
