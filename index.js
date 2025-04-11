6c88350622a06fbc5773c1a60669e5d93a6345bb
RISHIRAJ SINGH <rishiraj112004singh@gmail.com>
2025-04-11 10:57:24 +0530

    mongo commit

--- index.js
+++ index.js
@@ -10,20 +10,37 @@ app.use(
 app.use(express.static(__dirname));
 app.use(express.json());
 const path = require("path");
-const ethers = require('ethers');
+const { ethers } = require('ethers');
+const mongoose = require('mongoose');
 
 var port = 3000;
+async function connectToMongoDB() {
+    try {
+        const MONGO_URI = process.env.MONGO_URI;
+        if (!MONGO_URI) {
+            throw new Error("MONGO_URI is not defined in the .env file");
+        }
+        await mongoose.connect(MONGO_URI);
+        console.log("Connected to MongoDB successfully");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+        process.exit(1);
+    }
+}
 
+connectToMongoDB();
 const API_URL = process.env.API_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 const {abi} = require('./artifacts/contracts/Voting.sol/Voting.json');
+
 const provider = new ethers.providers.JsonRpcProvider(API_URL);
 
 const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 
 const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
